7d7dbcd4ac54b6bb712111367774ae41ccc56506
Hansen <samhansen.dev@gmail.com>
2025-02-27 19:39:24 +0300

    feat: add mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,32 +1,31 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    enabled = true, -- Ensure it's enabled
-    config = function()
-      require("mason").setup()
-    end,
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, {
-        "bash-language-server",
-        "typescript-language-server",
-        "black",
-        "goimports",
-        "golangci-lint",
-        "hadolint",
-        "isort",
-        "json-lsp",
-        "lua-language-server",
-        "markdownlint",
-        "prettier",
-        "pyright",
-        "shfmt",
-        "stylua",
-        "terraform-ls",
-        "tflint",
-        "yaml-language-server",
-      })
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		enabled = false, -- Ensure it's enabled
+		config = function()
+			require("mason").setup()
+		end,
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, {
+				"bash-language-server",
+				"typescript-language-server",
+				"black",
+				"goimports",
+				"golangci-lint",
+				"hadolint",
+				"isort",
+				"json-lsp",
+				"lua-language-server",
+				"markdownlint",
+				"prettier",
+				"pyright",
+				"shfmt",
+				"stylua",
+				"terraform-ls",
+				"tflint",
+				"yaml-language-server",
+			})
+		end,
+	},
 }
-
