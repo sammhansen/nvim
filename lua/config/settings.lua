949d8cf86a107c06acd468fea651d28570c65aca
Hansen <samhansen.dev@gmail.com>
2025-02-27 19:37:41 +0300

    prefs: add user settings

---
+++ lua/config/settings.lua
@@ -0,0 +1,9 @@
+vim.opt.tabstop = 2 -- Number of spaces that a tab character represents
+vim.opt.shiftwidth = 2 -- Number of spaces for auto-indentation
+-- vim.opt.expandtab = true -- Convert tabs to spaces
+
+-- turn on persistent undo
+vim.o.undofile = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
