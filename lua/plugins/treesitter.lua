8a6972830a44dc85a05f4a9abe0a56022757f22b
Hansen <samhansen.dev@gmail.com>
2025-02-28 23:08:49 +0300

    feat: add treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,34 +1,38 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    highlight = {
-      enable = true,
-    },
-    rainbow = {
-      enable = true,
-      extended_mode = true,
-      max_file_lines = 1000,
-    },
-    indent = {
-      enable = true,
-    },
-    ensure_installed = {
-      "help",
-      "typescript",
-      "javascript",
-      "bash",
-      "c",
-      "dockerfile",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "nix",
-      "python",
-      "regex",
-      "vim",
-      "vimdoc",
-      "yaml",
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	-- loads treesitter only while reading a file or creating a new one
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false, -- Prevents interference with Catppuccin
+			},
+			indent = {
+				enable = true,
+			},
+			rainbow = {
+				enable = true,
+				extended_mode = true,
+				max_file_lines = 1000,
+			},
+			ensure_installed = {
+				"bash",
+				"c",
+				"dockerfile",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"nix",
+				"python",
+				"regex",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
+		})
+	end,
 }
