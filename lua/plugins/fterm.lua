8d766f8b48b5aab2b4666b8d80ee47868c93763d
Hansen <samhansen.dev@gmail.com>
2025-02-28 21:35:00 +0300

    feat: add fterm

---
+++ lua/plugins/fterm.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"numToStr/FTerm.nvim",
+		config = function()
+			require("FTerm").setup({
+				border = "rounded",
+				dimensions = {
+					height = 0.9,
+					width = 0.9,
+				},
+			})
+		end,
+	},
+}
